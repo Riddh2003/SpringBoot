00c959429d5baf24453bc797fed816bb74c78e2d
Riddh Modi <riddhmodi2003@gmail.com>
2024-03-06 20:58:53 +0530

    Add FunRestController

---
+++ src/main/java/com/riddh/springboot/demo/myapplication/rest/FunRestController.java
@@ -0,0 +1,12 @@
+package com.riddh.springboot.demo.myapplication.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+}
