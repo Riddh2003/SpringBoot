594f7578f2d36a636afbaab38a7b68eead1a0671
Riddh Modi <riddhmodi2003@gmail.com>
2024-03-06 19:59:30 +0530

    Initial commit

---
+++ src/main/java/com/riddh/springboot/demo/myapplication/MyapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.riddh.springboot.demo.myapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riddh/springboot/demo/myapplication/MyapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riddh.springboot.demo.myapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
